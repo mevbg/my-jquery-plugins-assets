69e5e25b9dc7695a38b47f2132744077b8aa2afa
Martin Metodiev <martin@mev.bg>
2024-03-16 16:03:28 +0200

    Apply adaptations

--- configs/grunt/open.js
+++ configs/grunt/open.js
@@ -1,11 +1,11 @@
 module.exports = {
   build: {
     app: 'Google Chrome',
-    path: 'https://travis-ci.org/martinmethod/<%= pkg.name %>/builds'
+    path: 'https://travis-ci.org/mevbg/<%= pkg.name %>/builds'
   },
 
   repo: {
     app: 'Google Chrome',
-    path: 'https://github.com/martinmethod/<%= pkg.name %>'
+    path: 'https://github.com/mevbg/<%= pkg.name %>'
   }
 };
